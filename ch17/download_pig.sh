2cedc9d909ac0a6a109dfe6d2145347c4989ed3c
Alberto Paro <alberto.paro@gmail.com>
2022-03-12 22:40:40 +0100

    Ch17: added download of httpclient jar in pig script

--- ch17/download_pig.sh
+++ ch17/download_pig.sh
@@ -1,3 +1,4 @@
 wget -c https://downloads.apache.org/pig/pig-0.17.0/pig-0.17.0.tar.gz
+wget -c https://repo1.maven.org/maven2/commons-httpclient/commons-httpclient/3.1/commons-httpclient-3.1.jar
 tar xfvz pig-0.17.0.tar.gz
 
